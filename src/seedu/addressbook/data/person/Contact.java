fe0de70fb058dcc6d06d4092d41a630073e5af3d
Jun Wei Koh <Junwei@Junweis-MacBook-Pro.local>
2017-02-09 03:09:11 +0800

    parent class Contact created

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,30 @@
+package seedu.addressbook.data.person;
+
+public abstract class Contact {
+
+	public String value;
+	public boolean isPrivate;
+
+	@Override
+	public String toString() {
+		return value;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		return other == this // short circuit if same object
+				|| (other instanceof Address // instanceof handles nulls
+						&& this.value.equals(((Address) other).value)); // state
+																		// check
+	}
+
+	@Override
+	public int hashCode() {
+		return value.hashCode();
+	}
+
+	public boolean isPrivate() {
+		return isPrivate;
+	}
+
+}
